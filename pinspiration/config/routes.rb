547069e5fadfd60ba284c46ba53115ced19e9aa3
Liza Floyd <lizafloyd@gmail.com>
2016-10-10 13:38:28 -0400

    board crud functional

--- pinspiration/config/routes.rb
+++ pinspiration/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'boards/index'
+
+  get 'boards/show'
+
+  get 'boards/new'
+
+  get 'boards/create'
+
+  get 'boards/edit'
+
+  get 'boards/update'
+
+  get 'boards/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     resources :boards do
